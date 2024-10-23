74af44d477f7986b04b7b717c378e338e4a94f8f
Daniel Silveira <dsilveira32@gmail.com>
2024-10-23 22:48:01 +0100

    Funcao le pipe

--- Aula6/Exercicios/Ex1/src/main.c
+++ Aula6/Exercicios/Ex1/src/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+int le_pipe (int fd_in, char *msg_in, int bsize)
+{
+    int j;
+    for(j=0; j<bsize; j++) {
+        if (read (fd_in, &msg_in[j], 1) < 0) {
+            perror ("le_pipe read");
+            exit(1);
+        }
+        if (msg_in[j] == '\0')
+            break;
+    }
+    return j;
+}
+
+
 int main(int argc, char const *argv[])
 {
     printf("Hello World!\n");
